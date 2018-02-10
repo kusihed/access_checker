5291ce9ddf154d9b6cfce922b2b995d8020c4ca4
Markus Hediger <m.hed@gmx.ch>
2018-02-10 13:12:47 +0100

    init again

--- lib/access_checker.rb
+++ lib/access_checker.rb
@@ -20,4 +20,11 @@ module AccessChecker
     class SyntaxError  < ArgumentError; end
 end
 
+# coding: utf-8
+ActiveRecord::Base.class_eval do
+  include AccessChecker::Base
+end
+ActionController::Base.class_eval do
+  include AccessChecker::Control
+end
 
--- rails/init.rb
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-ActiveRecord::Base.class_eval do
-  include AccessChecker::Base
-end
-ActionController::Base.class_eval do
-  include AccessChecker::Control
-end
